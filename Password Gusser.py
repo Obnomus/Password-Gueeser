e00f7516d5ffd806d436519b6e6346a22d479d95
Obnomus <82671616+Obnomus@users.noreply.github.com>
2022-01-12 15:59:29 +0530

    Add files via upload

---
+++ Password Gusser.py
@@ -0,0 +1,32 @@
+# importing random 
+from random import *
+
+#taking input from user
+user_pass=input("Enter your password")
+
+# storing alphabet letter to use thm to crack password 
+password = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', '1', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', '2',]
+
+# initializing an empty string
+
+guess = ""
+
+# using while loop to generate many passwords untill one of #them does not matches user_pass
+
+while (guess != user_pass):
+
+   guess =""
+
+# generating random passwords using for loop 
+for letter in range(len(user_pass)):
+    
+    guess_letter = password [randint(0, 25)] 
+    
+    guess = str(guess_letter) + str(guess)
+
+#printing guessed passwords
+
+print(guess)
+
+#printing the matched password 
+print("Your password is", guess)
